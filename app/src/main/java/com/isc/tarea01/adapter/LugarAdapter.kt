f0b25ed0af44ecad2f845f17c9c1cd8c40ae55ff
Joseph <yoyoss3248>
2022-04-03 23:14:14 -0600

    Semana4 Actualizar/Delete de Cards

--- app/src/main/java/com/isc/tarea01/adapter/LugarAdapter.kt
+++ app/src/main/java/com/isc/tarea01/adapter/LugarAdapter.kt
@@ -1,27 +1,54 @@
 package com.isc.tarea01.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.isc.tarea01.databinding.FragmentAddLugarBinding
 import com.isc.tarea01.databinding.LugarFilaBinding
+import com.isc.tarea01.model.Lugar
+import com.isc.tarea01.ui.lugar.LugarFragmentDirections
 
 class LugarAdapter : RecyclerView.Adapter<LugarAdapter.LugarViewHolder>() {
 
+    private var listaLugares = emptyList<Lugar>()
+
+
     inner class LugarViewHolder(private val itemBinding: LugarFilaBinding)
         : RecyclerView.ViewHolder(itemBinding.root){
+            fun bind(lugar: Lugar){
+                itemBinding.tvNombre.text = lugar.nombre
+                itemBinding.tvCorreo.text = lugar.correo
+                itemBinding.tvTelefono.text = lugar.telefono
+                itemBinding.tvWeb.text = lugar.web
+                itemBinding.vistaFila.setOnClickListener {
+                    val accion = LugarFragmentDirections.actionNavLugarToNavUpdateLugar(lugar)
+                    itemView.findNavController().navigate(accion)
+                }
+
+            }
 
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LugarViewHolder {
-        TODO("Not yet implemented")
+
+        //Crea una tarjeta
+        val itemBinding = LugarFilaBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return LugarViewHolder(itemBinding)//devuelvo la card para pintar
     }
 
     override fun onBindViewHolder(holder: LugarViewHolder, position: Int) {
-        TODO("Not yet implemented")
+       val lugar = listaLugares[position]//devuelvo el lugar donde pintar
+        holder.bind(lugar) //se invoca la funcion del inner class
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return listaLugares.size //Para saber la cantidad de cards que debe de realizar
+    }
+
+    fun setData(lugares : List<Lugar>){
+        this.listaLugares = lugares
+        notifyDataSetChanged()
     }
 
 }
--- app/src/main/java/com/isc/tarea01/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/isc/tarea01/ui/lugar/LugarFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.isc.tarea01.R
+import com.isc.tarea01.adapter.LugarAdapter
 import com.isc.tarea01.databinding.FragmentLugarBinding
 import com.isc.tarea01.viewmodel.LugarViewModel
 
@@ -33,6 +36,26 @@ class LugarFragment : Fragment() {
 binding.floatingActionButton.setOnClickListener {
     findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
 }
+//activar el recycler view
+        var lugarAdapter = LugarAdapter() //se crea un objeto adapter
+        var reciclador = binding.reciclador //se recupera el recycler view de la vista
+
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+
+        //Se crea un observador que me muestra toda la info de la tabla lugares
+        //se actualiza cuando cambio el set de datos
+        lugarViewModel.getAllData.observe(viewLifecycleOwner){ lugares ->
+
+            lugarAdapter.setData(lugares)
+        }
+
+
+
+
+
+
 
         return binding.root
     }
